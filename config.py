952b02959d1c271bb192146902bbe219540aeca0
bingwang619@gmail.com <bingwang619@gmail.com>
2017-08-18 15:56:42 +0800

    delete config file

--- config.py
+++
@@ -1,4 +0,0 @@
-NOTIFY_URL = ("https://oapi.dingtalk.com/robot/send?access_token="
-    "c6d5a2936381dfc29394f3c336bea5fad962d90ffd31809e92d95a1xxxxxxxxx")
-MOBILE_NUMBER = "176xxxxx619"
-HOST_IP = "127.0.0.1"
